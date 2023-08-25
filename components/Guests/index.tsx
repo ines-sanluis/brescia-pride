c97a98d80a32edf03666ad4494dc88531ba7c079
ines <insanlur@gmail.com>
2023-08-25 23:45:00 +0200

    Add pronouns

--- components/Guests/index.tsx
+++ components/Guests/index.tsx
@@ -1,38 +1,55 @@
-import React from 'react'
+import React from "react";
 
 const speeches = [
   {
     name: "Student3 scuole di Brescia",
-    speech: "Una proiezione del futuro con le rivendicazioni di UDS Brescia e dei collettivi degli istituti De Andrè, Arnaldo, Gambara e Gigli."
+    speech:
+      "Una proiezione del futuro con le rivendicazioni di UDS Brescia e dei collettivi degli istituti De Andrè, Arnaldo, Gambara e Gigli.",
   },
   {
     name: "Famiglie Arcobaleno + Agedo",
-    speech: "Per ricordare che non esiste un solo tipo di famiglia, parlerà dell’importanza della lotta per il riconoscimento giuridico e sociale di ogni tipo di genitorialità e s/famiglia."
+    speech:
+      "Per ricordare che non esiste un solo tipo di famiglia, parlerà dell’importanza della lotta per il riconoscimento giuridico e sociale di ogni tipo di genitorialità e s/famiglia.",
   },
   {
     name: "Annalisa Sirignano",
-    speech: "Speaker e giornalista orgogliosamente terrona, è parte di Ti leggiamo una femminista e \"Ccà nisciun' è fessa\", rete territoriale che mappa l'accesso all'interruzione volontaria di gravidanza e sensibilizza sulla salute sessuale.",
+    pronouns: ["she", "her"],
+    speech:
+      "Speaker e giornalista orgogliosamente terrona, è parte di Ti leggiamo una femminista e \"Ccà nisciun' è fessa\", rete territoriale che mappa l'accesso all'interruzione volontaria di gravidanza e sensibilizza sulla salute sessuale.",
   },
   {
     name: "Non Una Di Meno Brescia",
-    speech: "Movimento politico transfemminista, antirazzista, antifascista e anticapitalista. Porterà la sua voce collettiva e orizzontale sulla creazione di percorsi intersezionali di lotta e azioni che intervengano sulle molteplici forme di violenza strutturale ed eterocispatriarcale."
+    speech:
+      "Movimento politico transfemminista, antirazzista, antifascista e anticapitalista. Porterà la sua voce collettiva e orizzontale sulla creazione di percorsi intersezionali di lotta e azioni che intervengano sulle molteplici forme di violenza strutturale ed eterocispatriarcale.",
   },
   {
     name: "Kay Kamakhya",
-    speech: "Attivista indiana per i diritti umani e la tutela dell'ambiente. Kay, che si identifica come trans feminine, rende il suo lavoro nella moda un mezzo per battersi per la giustizia sociale.  Intervento in inglese con traduzione italiana."
-  }
-]
+    pronouns: ["she", "her"],
+    speech:
+      "Attivista indiana per i diritti umani e la tutela dell'ambiente. Kay, che si identifica come trans feminine, rende il suo lavoro nella moda un mezzo per battersi per la giustizia sociale.  Intervento in inglese con traduzione italiana.",
+  },
+];
 
-export default function Guests()  {
+export default function Guests() {
   return (
     <>
-    {speeches.map((speech, index) => (
-      <div key={`speech-${index}`}>
-        <h3>{speech.name}</h3>
-        <p>{speech.speech}</p>
-      </div>
-    ))}
-
+      {speeches.map((speech, index) => (
+        <div className="speech" key={`speech-${index}`}>
+          <h3>
+            <span>{speech.name}</span>
+            {speech.pronouns && <span className="pronouns">{speech.pronouns.join("/")}</span>}
+          </h3>
+          <p>{speech.speech}</p>
+        </div>
+      ))}
+      <style jsx>{`
+        .speech h3 
+        .pronouns {
+          margin-left: 10px;
+          font-family: inherit;
+          font-size: 0.8em;
+        }
+      `}</style>
     </>
-  )
+  );
 }
